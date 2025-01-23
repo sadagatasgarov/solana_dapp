94b66d921d19700da41a839816fef1d6d2b1c281
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-01-23 23:00:43 +0400

    ts yerinse js yazdiq ama ts de isledi????

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -4,14 +4,36 @@ declare_id!("Foc5ecG5hHTeapmBvQn9GibQmRYLFTTMH6iZFF6XDuRV");
 
 #[program]
 pub mod mycalculatordapp {
-    use anchor_lang::solana_program::entrypoint::ProgramResult;
+    //use anchor_lang::solana_program::entrypoint::ProgramResult;
 
     use super::*;
 
-    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+    pub fn create(ctx: Context<Create>, init_message: String) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.greeting = init_message;
         Ok(())
     }
 }
 
+
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer=user, space=264)]
+    pub calculator: Account<'info, Calculator>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+
+}
+
+
+
+#[account]
+pub struct Calculator {
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64
+}
--- tests/mycalculatordapp.ts
+++ tests/mycalculatordapp.ts
@@ -1,16 +1,27 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Mycalculatordapp } from "../target/types/mycalculatordapp";
+const assert = require('assert')
+const anchor = require('@coral-xyz/anchor')
+const {SystemProgram} = anchor.web3
 
-describe("mycalculatordapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe('mycalculatorapp', () => {
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider)
+  const calculator = anchor.web3.Keypair.generate()
 
-  const program = anchor.workspace.Mycalculatordapp as Program<Mycalculatordapp>;
+  //console.log(generate)
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  const program = anchor.workspace.Mycalculatordapp
+
+  it('Creates a calculator', async() => {
+    await program.rpc.create("Welcome to Solana", {
+      accounts: {
+        calculator: calculator.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [calculator]
+    })
+
+    const account = await program.account.calculator.fetch(calculator.publicKey)
+    assert.ok(account.greeting === "Welcome to Solana")
+  })
+})
